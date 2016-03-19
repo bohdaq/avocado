bda661fd8912c1b0d1d4cca225efdd484aaa41da
Bogdan Tsap <bogdan.tsap@thinktank.net>
2016-03-19 02:51:50 +0200

    clean up

--- dev/type.directive.ts
+++ dev/type.directive.ts
@@ -16,18 +16,13 @@ export class TypeDirective implements OnInit {
     ngOnInit():any {
         console.log(this.type);
         if(this.type === "datepicker"){
-            console.log("inside")
             this._elRef.nativeElement.dataset.provide = "datepicker";
             setInterval(() => this.datepickerUpdates.emit(this._elRef.nativeElement.value), 1000);
+            return;
+        } else if (this.type === "moneypicker"){
+            this._elRef.nativeElement.type = "number";
             return;
         }
         this._elRef.nativeElement.type = this.type || this._defaultType;
     }
-
-    // updateDatepickerValue(): any {
-    //     console.log(this._elRef.nativeElement.value);
-    //
-    // }
-
-
 }
