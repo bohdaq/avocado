3efd9e8b9773ce4225e6650166a085c3dfe26a7c
Bogdan Tsap <bogdan.tsap@thinktank.net>
2016-03-19 02:35:01 +0200

    found a way to catch datepicker updates

--- dev/type.directive.ts
+++ dev/type.directive.ts
@@ -1,10 +1,10 @@
-import {Directive, ElementRef, OnInit} from "angular2/core";
+import {Directive, ElementRef, OnInit, AfterContentChecked} from "angular2/core";
 
 @Directive({
     selector: '[myType]',
     inputs: ['type:myType']
 })
-export class TypeDirective implements OnInit {
+export class TypeDirective implements OnInit, AfterContentChecked {
     private _defaultType: string = 'text';
     type: string;
 
@@ -18,4 +18,13 @@ export class TypeDirective implements OnInit {
         }
         this._elRef.nativeElement.type = this.type || this._defaultType;
     }
+
+    ngAfterContentChecked() {
+        if(this.type === "datepicker"){
+            this._elRef.nativeElement.dataset.provide = "datepicker";
+            console.log(this._elRef.nativeElement.value);
+            return;
+        }
+    }
+
 }
